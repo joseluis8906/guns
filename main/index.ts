82649d5545eaa3206ecc2c9f2657d9acc5171209
Jose Luis Cáceres Escudero <joseluiscacere8906@gmail.com>
2018-12-20 19:23:58 -0500

    On branch master: se corrigen bugs.
    Changes to be committed:
    	modified:   main/index.ts

--- main/index.ts
+++ main/index.ts
@@ -3,3 +3,7 @@ import express from "express"
 const app = express();
 
 app.listen(3000, () => console.log("Server running on 3000"));
+
+app.get("/", (req, res) => {
+  res.send("Awesome! We're live debugging this!")
+});
